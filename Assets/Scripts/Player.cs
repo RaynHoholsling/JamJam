0a1759dbd663fe4079809366ba18b76e36a1e493
RaynHoholsling <gns2008sng@gmail.com>
2024-07-13 02:17:00 +0300

    1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool facingRight = true;
+    [SerializeField] private GameObject doors;
+
+    Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (!facingRight && mousePosition.x > transform.position.x)
+        {
+            Flip();
+        }
+        else if (facingRight && mousePosition.x < transform.position.x)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (gameObject == null)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        //Vector3 WeaponScaler = GameObject.FindGameObjectWithTag("Weapon").GetComponent<Transform>().localScale;
+        //Scaler.x *= -1;
+        //transform.localScale = Scaler;
+        //WeaponScaler.x *= -1;
+        //WeaponScaler.y *= -1;
+        //GameObject.FindGameObjectWithTag("Weapon").GetComponent<Transform>().localScale = WeaponScaler;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("DoorTrigger"))
+    //    {
+    //        doors.SetActive(true);
+    //    }
+
+    //    if (other.CompareTag("Health"))
+    //    {
+    //        GetComponent<Health>().health += 50;
+    //        GameObject.FindGameObjectWithTag("HP").GetComponent<Image>().fillAmount += 0.5f;
+    //        Destroy(other.gameObject);
+    //    }
+    //}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float velocity;
+    public int damage;
+
+    private void Update()
+    {
+        if ((transform.position.x >= 100 || transform.position.x <= -100) || (transform.position.y >= 100 || transform.position.y <= -100))
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if((collision.collider != null) && (collision.collider.CompareTag("Player") == false) && (collision.collider.CompareTag("PlayerProjectile") == false))
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                collision.gameObject.GetComponent<Health>().TakeDamage(damage); 
+            }
+            Destroy(gameObject);                
+        }
+    }
+}
