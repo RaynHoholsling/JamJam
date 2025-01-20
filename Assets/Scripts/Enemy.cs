b22edc1989dd136505bd36b592b44ca8cc97d98f
arshutz <inokentijinokentiev6@gmail.com>
2025-01-20 11:22:47 +0100

    fdasfsdf

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,11 +20,14 @@ public class Enemy : MonoBehaviour
     [SerializeField]  private float waitTime;
     public float startWaitTime;
     [SerializeField] private bool isGoingBack;
+    [SerializeField] private float _rayCastDistance;
     [SerializeField] private bool _playerSpotted;
     [SerializeField] private bool _isInvestigating;
     [SerializeField] private bool _isSeeingPlayer;
     [SerializeField] private Sprite deadSprite;
     [SerializeField] private GameObject _deadBody;
+    [SerializeField] private GameObject _rayObject;
+    private int _rayScaler = 1;
     int i = 0;
 
 
@@ -44,23 +47,18 @@ public class Enemy : MonoBehaviour
         }
         animator = GetComponent<Animator>();
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            _playerSpotted = true;
-            if (_isInvestigating == false)
-            {
-                StartCoroutine(Investigating());
-            }
-            if (_isSeeingPlayer == false)
-            {
-                StartCoroutine(SeeingPlayer());
-            }
-        }
-    }
 
-    private void OnTriggerExit2D(Collider2D collision)
+
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "Player")
+    //    {
+
+//    } 
+//}       FOR RAYCAST 
+
+private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
@@ -119,23 +117,53 @@ public class Enemy : MonoBehaviour
             }
         }
     }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hitPlayer = Physics2D.Raycast(_rayObject.transform.position, Vector2.right * _rayScaler);
+        RaycastHit2D hitPlayerUp = Physics2D.Raycast(_rayObject.transform.position, _rayScaler * 0.5f * Vector2.up);
+        RaycastHit2D hitPlayerDown = Physics2D.Raycast(_rayObject.transform.position, Vector2.down * _rayScaler * 0.5f);
+        Debug.DrawRay(_rayObject.transform.position, Vector2.right * _rayCastDistance * _rayScaler, Color.red);
+        Debug.DrawRay(_rayObject.transform.position, Vector2.up * _rayCastDistance * _rayScaler * 0.5f, Color.red);
+        Debug.DrawRay(_rayObject.transform.position, Vector2.down * _rayCastDistance * _rayScaler * 0.5f, Color.red);
+
+        if (hitPlayer.collider.tag == "Player" || hitPlayerUp.collider.tag == "Player" || hitPlayerDown.collider.tag == "Player")
+        {
+            Debug.Log("fdfdfsdf");
+            _playerSpotted = true;
+            if (_isInvestigating == false)
+            {
+                StartCoroutine(Investigating());
+            }
+            if (_isSeeingPlayer == false)
+            {
+                StartCoroutine(SeeingPlayer());
+            }
+        }
+    }
+
     void Flip()
     {
         facingRight = !facingRight;
         Vector3 Scaler = transform.localScale;
+        _rayScaler *= -1;
         Scaler.x *= -1;
         transform.localScale = Scaler;
     }
 
+    
+   
+
     IEnumerator SeeingPlayer()
     {
+        Debug.Log("Player spotted");
         _isSeeingPlayer = true;
         yield return new WaitForSeconds(1);
 
         if (_playerSpotted)
         {
-            Debug.Log("Player spotted");
-            Destroy(Player);
+           
+            Debug.Log("Player killed");
         }
 
         _isSeeingPlayer = false;
